b96842ee94cb1fdac31c79b0327746be9d7b9c92
eol nuha <olinuha08@hotmail.com>
2023-03-04 02:54:01 +0100

    init

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className={clsx("hero__title", styles.heroTitle)}>THE INNOHIVE</h1>
+        <p className={clsx("hero__subtitle", styles.heroTitle)}>Blog</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <>
+      <Head>
+        <body className="homepage" />
+      </Head>
+      <Layout
+        title={`Hello from ${siteConfig.title}`}
+        description="Description will go into a meta tag in <head />">
+        <HomepageHeader />
+        <main>
+          <HomepageFeatures />
+        </main>
+      </Layout>
+    </>
+  );
+}
